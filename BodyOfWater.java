bb805771124be3123b781317c973ba9bcd019ed1
Jihoon <aidenjh26@ausdk12.org>
2025-08-22 13:46:48 -0700

    Body of Water work

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,21 @@
+public class BodyOfWater{
+    private String name;
+    private int largestDiameter;
+    private int avgDepth;
+    private boolean isSaltWater;
 
+    public BodyOfWater(String n, int d, int p, boolean s){
+        name = n;
+        largestDiameter = d;
+        avgDepth = p;
+        isSaltWater = s;
+        System.out.println("My name is: " + name);
+        System.out.println("The largest diameter is: " + largestDiameter);
+        System.out.println("The average Depth is: " + avgDepth);
+        if (isSaltWater) {
+            System.out.println("The water is salty.");
+        } else {
+            System.out.println("The water is not salty.");
+        }
+    }
+}
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,6 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Lake Superior", 1848000, 483, false);
+        b.BodyOfWater();
     }
 }
