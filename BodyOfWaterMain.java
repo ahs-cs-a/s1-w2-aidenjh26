9a1bda12541a88f67f2a410019de4ff59b79dd66
aidenjh26 <aidenjh26@ausdk12.org>
2025-08-22 16:06:35 -0700

    BodyOfWaterMain.java

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,10 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
         BodyOfWater b = new BodyOfWater("Lake Superior", 1848000, 483, false);
-        b.BodyOfWater();
+        BodyOfWater b2 = new BodyOfWater("Caspian Sea", 690);
+        b.printState();
+        b2.printState();
+        b2.setName("Fake Sea");
+        b2.printState();
     }
 }
